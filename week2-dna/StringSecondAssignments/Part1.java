d985b20099e62b62fa024f5494f615206b30cfb9
Ryan Juza <ryjz@pm.me>
2019-06-12 17:13:28 -0400

    Updated findStopCodon, tested and working. Looping next.

--- week2-dna/StringSecondAssignments/Part1.java
+++ week2-dna/StringSecondAssignments/Part1.java
@@ -13,23 +13,21 @@ public class Part1 {
             stopCodon = stopCodon.toLowerCase();
         }
         
-                //if startIndex is -1, returns dna string
-        if (startIndex == -1){
-            return startIndex;
-        }
-        
         //Finds the index position of the first stop codon, appearing after the SsartIndex that was found (the 'start+3').
         //If the length of the substring between the startIndex and stopCodon is a multiple of 3,
         //returns the index of the stopCodon
         int stop = dna.indexOf(stopCodon, startIndex+3);
         
-        if ((stop - startIndex) % 3 == 0){
-            return dna.indexOf(stopCodon);
+        if (startIndex != -1){
+            if ((stop - startIndex) % 3 == 0){
+            //returns index of the stop codon, starting AFTER the start codon
+            return dna.indexOf(stopCodon, startIndex+3);
+            }
+            //If there is no such stopCodon, return the length of the dna strand
+            else{return dna.length();}
         }
-        //If there is no such stopCodon, return the length of the dna strand
-        else {         
-                return dna.length();
-        }
+        //if startIndex is -1, returns dna string
+        else{return startIndex;}
     }
     
     public void testFindStopCodon(){
@@ -60,15 +58,59 @@ public class Part1 {
     }
     
     public String findGene(String dna){
+            String gene;
             String startCodon = "ATG";
             //Checks if the 'dna' string is lowercase. If so, makes the exampleStartCodon lower case as well
-            if(startCodon.equals(startCodon.toLowerCase())){
+            if(dna.equals(dna.toLowerCase())){
             startCodon = "atg";
             }
             //Finds the index position of the start codon “ATG”.
             int startIndex = dna.indexOf(startCodon);
-            String stopCodon ="TAA";
-            int stopIndexTAA = findStopCodon(dna, startIndex, stopCodon);     
+            
+            if(startIndex != -1){
+                int stopIndexTAA = findStopCodon(dna, startIndex, "TAA");
+                int stopIndexTAG = findStopCodon(dna, startIndex, "TAG");
+                int stopIndexTGA = findStopCodon(dna, startIndex, "TGA");
+
+                if((stopIndexTAA < stopIndexTAG) && (stopIndexTAA < stopIndexTGA)){
+                    gene = dna.substring(startIndex, stopIndexTAA+3);
+                }
+                else if((stopIndexTAG < stopIndexTAA) && (stopIndexTAG < stopIndexTGA)){
+                    gene = dna.substring(startIndex, stopIndexTAG+3);
+                }
+                else if((stopIndexTGA < stopIndexTAA) && (stopIndexTGA < stopIndexTAG)){
+                    gene = dna.substring(startIndex, stopIndexTGA+3);
+                }
+                else{gene = dna + " has ATG, but no gene";}
+            }
+            else{return dna + " has no ATG";}
+            return gene;
+    }
+    
+    public void testFindGene(){
+        //test values for findGene method
+        String[] geneExamples= new String[]{
+        //DNA with no “ATG”
+        "CCCAAAGGGTTTTTTAAA",
+        //DNA with “ATG” and one valid stop codon
+        "CCCATGCCCAAATTTGGGTAA",
+        //DNA with “ATG” and multiple valid stop codons
+        "atgaaatttgggtagccctttgggtaagggtgaccc",
+        //DNA with “ATG” and no valid stop codons
+        "atggggtttaaaccccctttt",
+        //DNA with "ATG" and a valid stop codon, but not multiple of 3
+        "CCCATGCCCAAATTTGGGGTAAGGG"};
+        
+        for(int i = 0; i<geneExamples.length; i++){
+        //prints out the restults of each example
+        System.out.println("Result for " + geneExamples[i] + " = " + findGene(geneExamples[i]));
+        }
+    }
+    
+    public void printAllGenes(String dna){
+        //repeatedly find genes and 
+        //print each one 
+        //until there are no more genes
     }
 }
 
