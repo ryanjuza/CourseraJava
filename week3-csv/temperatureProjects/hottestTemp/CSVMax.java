cbe85f7a7503786be799175b36ab192bb7693681
ryanjuza <ryjz@pm.me>
2020-01-02 16:39:00 -0500

    Added hottest day of many files

--- week3-csv/temperatureProjects/hottestTemp/CSVMax.java
+++ week3-csv/temperatureProjects/hottestTemp/CSVMax.java
@@ -32,10 +32,40 @@ public class CSVMax {
     }
 
     public void testHottestInDay () {
-		FileResource fr = new FileResource("data/2015/weather-2015-01-01.csv");
-		CSVRecord largest = hottestHourInFile(fr.getCSVParser());
-		System.out.println("hottest temperature was " + largest.get("TemperatureF") +
-				   " at " + largest.get("TimeEST"));
-	}
+        FileResource fr = new FileResource("data/2015/weather-2015-01-01.csv");
+        CSVRecord largest = hottestHourInFile(fr.getCSVParser());
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") +
+        " at " + largest.get("TimeEST"));
+    }
+    
+    public CSVRecord hottestInManyDays(){
+        CSVRecord largestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        //iterate over files
+        for(File f : dr.selectedFiles()){
+            FileResource fr = new FileResource(f);
+            //get largest in file
+            CSVRecord currentRow = hottestHourInFile(fr.getCSVParser());
+            if(largestSoFar == null){
+                largestSoFar = currentRow;
+            }
+            else{
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+                //Check if currentRow’s temperature > largestSoFar’s
+                if (currentTemp > largestTemp) {
+                    //If so update largestSoFar to currentRow
+                    largestSoFar = currentRow;
+            }
+            }
+        }
+        return largestSoFar;
+       }
+       
+       public void testHottestInManyDays () {
+        CSVRecord largest = hottestInManyDays();
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") +
+        " at " + largest.get("DateUTC"));
+    }
 }
 
